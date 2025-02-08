652d42825366ae2e2af2d208164a885702b7ad0a
Vasanth Kumar <mybusiness.vkrs@gmail.com>
2025-02-08 09:04:08 +0530

    API work completed

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
 const search_btn = document.querySelector(".horizontal_content_2");
 
+let cityname = "Chennai";
+checkweather(cityname);
+
+function kelvinToCelsius(kelvin) {
+  return kelvin - 273.15;
+}
+
+async function checkweather(cityname) {
+  const api_key = "6a313d4d3a7492e4c6758db3bc0e74f5";
+
+  const api_url = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&units=metrics&`;
+
+  const response = await fetch(api_url + `appid=${api_key}`);
+
+  const rsp_msg = await response.json();
+
+  console.log(rsp_msg);
+
+  document.querySelector(".city").innerHTML = rsp_msg.name;
+
+  let kelvinTemp = rsp_msg.main.temp;
+  let celsiusTemp = kelvinToCelsius(kelvinTemp);
+
+  document.querySelector(".city_temp").innerHTML = `${celsiusTemp.toFixed(
+    2
+  )}&#x2103`;
+
+  document.querySelector(
+    ".city_humidity"
+  ).innerHTML = `${rsp_msg.main.humidity}%`;
+
+  document.querySelector(".city_speed").innerHTML = `${rsp_msg.wind.speed}Km/h`;
+
+  if (rsp_msg.weather[0].main == "Mist" || rsp_msg.weather[0].main == "Fog") {
+    document.querySelector(".weather_img").src = "images/fog.png";
+  } else if (rsp_msg.weather[0].main == "Clear") {
+    document.querySelector(".weather_img").src = "images/sun.png";
+  } else if (rsp_msg.weather[0].main == "Cloudy") {
+    document.querySelector(".weather_img").src = "images/cloudy (1).png";
+  } else if (rsp_msg.weather[0].main == "Rain") {
+    document.querySelector(".weather_img").src = "images/heavy-rain.png";
+  } else if (rsp_msg.weather[0].main == "Snow") {
+    document.querySelector(".weather_img").src = "images/snow.png";
+  } else if (rsp_msg.weather[0].main == "Haze") {
+    document.querySelector(".weather_img").src = "images/haze.png";
+  }
+}
+
 search_btn.addEventListener("click", () => {
   let cityname = document.querySelector(".input_text"); // Retrieve value inside the event listener
+  checkweather(cityname.value);
   cityname.value = "";
 });
